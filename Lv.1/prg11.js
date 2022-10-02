1cb2da395aa7c216fc58823000c0bf94fad97a24
jooyonee <isyawn@naver.com>
2022-10-03 01:40:38 +0900

    분수의 덧셈

--- Lv.1/prg11.js
+++ Lv.1/prg11.js
@@ -1,25 +1,25 @@
-var denum1 = 1;
-var num1 = 2;
-var denum2 = 3;
-var num2 = 4;
+var denum1 = 12;
+var num1 = 1;
+var denum2 = 24;
+var num2 = 1;
 
     var answer = [];
+
     
     //분모 최소공배수(x) 구하기
-
 	var i = 1;
     while(true){
       if((i % num1 === 0) && (i % num2 === 0)){
         break;
       }
       i++;
-      var mom = i;
     }
+    var mom = i;
     
-    var son = denum1*(mom/num1) + denum2*(mom/num2);
+    var son = denum1*parseInt(mom/num1) + denum2*parseInt(mom/num2);
+
 
     //분모와 분자의 최대공약수(y) 구하기
-
     var i = 1;
     while(i <= mom && i <= son) {
         if (mom % i === 0 && son % i === 0) {
@@ -27,7 +27,6 @@ var num2 = 4;
         }
         i++;
     }
-    
     son = son / y;
     mom = mom / y;
 
