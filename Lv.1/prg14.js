52e1eeffd28df420b1f9b487d8e370c6d4e06089
jooyonee <isyawn@naver.com>
2022-10-05 21:36:07 +0900

    직각삼각형 출력하기

---
+++ Lv.1/prg14.js
@@ -0,0 +1,18 @@
+var angle = 70;
+
+    
+    if (angle < 90) {
+        console.log(1);
+    }
+
+    if (angle == 90) {
+        console.log(2);
+    }
+
+    if (90 < angle && angle < 180) {
+        console.log(3);
+    }
+
+    if (angle == 180) {
+        console.log(4);
+    }
