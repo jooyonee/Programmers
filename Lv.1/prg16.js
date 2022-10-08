2c9c548d9c786608c034aca04ae1de04d0942d8f
jooyonee <isyawn@naver.com>
2022-10-08 12:09:14 +0900

    진료순서 정하기

--- Lv.1/prg16.js
+++ Lv.1/prg16.js
@@ -7,8 +7,15 @@ var arr2 = emergency.slice();
 
 //오름차순으로 arr2변형
 var arr = arr2.sort(function(a, b) {
-    a-b;
+    return a-b;
 });
 
-console.log(arr);
-
+var answer = [];
+for (var i = 0; i<arr.length; i++) {
+    for (var j = 0; j<arr.length; j++) {
+        if (emergency[i] === arr[j]) {
+            answer.push(j+1);
+        }
+    }
+}
+console.log(answer);
