766652287df19e8e1ec460b13d638d62e2363ead
jooyonee <isyawn@naver.com>
2022-10-08 15:16:19 +0900

    개미군단

---
+++ Lv.1/prg18.js
@@ -0,0 +1,16 @@
+// 개미군단
+var hp = 24;
+
+var ant = 0;
+
+var jang = parseInt(hp / 5);
+hp = hp % 5;
+console.log(jang);
+
+var byeong = parseInt(hp / 3);
+hp = hp % 3;
+console.log(byeong);
+
+var il = parseInt(hp % 3);
+console.log(il);
+
