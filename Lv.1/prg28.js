1d89d9f19f531cd882d0522a3159465761f3ee63
jooyonee <isyawn@naver.com>
2022-10-10 16:22:54 +0900

    문자열 정렬하기

---
+++ Lv.1/prg28.js
@@ -0,0 +1,7 @@
+var my_string = "p2o4i8gj2";
+var str_arr = my_string.split('');
+
+for(var i = 0; i<str_arr.length; i++) {
+    str_arr[i] = Number(str_arr[i]);
+}
+var num_arr = str_arr.filter(n => isNaN(n) === false);
