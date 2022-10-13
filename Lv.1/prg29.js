ce50999488d5f0b558c2101644cf9cf0f98da1fe
jooyonee <isyawn@naver.com>
2022-10-13 23:45:45 +0900

    컨트롤 제트

--- Lv.1/prg29.js
+++ Lv.1/prg29.js
@@ -1,23 +1,20 @@
 //컨트롤 제트
 
-var s = "0 1 4 9 Z Z";
+var s = "0 1 4 9 Z Z 3";
 
 var answer = 0;
 
 var s_arr = s.split(' ');
  
 for (var i = 0; i<s_arr.length; i++) {
-    
-    var j = 0;
-    while(s_arr[i] != 'Z' && j<s_arr.length) {
-        answer += parseInt(s_arr[i]);
-
-        if(s_arr[i+1] === 'Z') {
-            answer -= parseInt(s_arr[i]);
-        }
-        j++;
+    if (s_arr[i+1] === 'Z') {
+        s_arr.splice(i, 2);
+        i = 0;
     }
-    
+}
+var answer = 0;
+for (var i = 0; i<s_arr.length; i++) {
+    answer += parseInt(s_arr[i]);
 }
 
 console.log(answer);
