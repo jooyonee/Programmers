fd0d3142c85ed531a9f220e1272d46275f33131f
jooyonee <isyawn@naver.com>
2022-10-15 20:57:39 +0900

    소인수분해

---
+++ Lv.1/prg32.js
@@ -0,0 +1,17 @@
+//소인수분해
+
+function solution(n) {
+    var answer = [];
+
+    for(let i = 2; i <= n; i++) {
+
+        while (n % i === 0) {
+
+            n = n / i;
+            answer.push(i);
+
+        }
+    }
+
+    return [...new Set(answer)];
+}
