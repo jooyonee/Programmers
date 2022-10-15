1868511ec16b6befc028f29cf17ae463b7d95a75
jooyonee <isyawn@naver.com>
2022-10-15 20:49:18 +0900

    문자열 안에 문자열

--- Lv.1/prg29.js
+++ Lv.1/prg29.js
@@ -1,20 +1,28 @@
 //컨트롤 제트
 
-var s = "0 1 4 9 Z Z 3";
+var s = "10 Z 20 Z 1";
 
 var answer = 0;
 
 var s_arr = s.split(' ');
  
-for (var i = 0; i<s_arr.length; i++) {
+
+var i = 0;
+while (i<s_arr.length) {
     if (s_arr[i+1] === 'Z') {
         s_arr.splice(i, 2);
         i = 0;
     }
+    else {
+        i ++;
+    }
 }
+
 var answer = 0;
 for (var i = 0; i<s_arr.length; i++) {
     answer += parseInt(s_arr[i]);
 }
 
 console.log(answer);
+
+
---
+++ Lv.1/prg31.js
@@ -0,0 +1,4 @@
+//문자열 안에 문자열
+function solution(str1, str2) {
+    return str1.includes(str2) ? 1 : 2;
+}
