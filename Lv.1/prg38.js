b30a2d5d4213e9d85658d82bd383bf108be5db79
jooyonee <isyawn@naver.com>
2022-10-16 22:52:39 +0900

    영어가 싫어요

--- Lv.1/prg38.js
+++ Lv.1/prg38.js
@@ -1,32 +1,40 @@
 //영어가 싫어요
 
 var numbers = "onefourzerosixseven";
-// var numbers = numbers.split('');
 
 var num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
-    var answer = [];
-
-    var i = 0;
-    while (numbers.length != 0) {
-        var ind = numbers.indexOf(num[i]);
-        console.log(numbers);
-
-        numbers = numbers.toString().split(',');
-
-        if (ind != -1) {
-            numbers.splice(ind, num[i].length);
-            answer.push(i);
-        }
-        i++;
-        numbers.join('');
-        console.log(numbers);
-
-    }
-console.log(answer);
-//     var ind = numbers.indexOf(num[1]);
-//     var numbers = numbers.split('');
-//     var arr = numbers.splice(ind, num[1].length);
-
     
 
-// console.log(numbers);
+    var answer = [];
+    for(var i = 0; i < num.length; i++) {
+        if ( numbers.indexOf(num[i]) != -1) {
+            answer[numbers.indexOf(num[i])] = i;
+        }
+        
+    }
+    console.log(Number(answer.join('')));
+
+
+//첫번째 풀이**
+    // var answer = [];
+    // var i = 0;
+    // while (numbers.length > 0) {
+    //     var ind = numbers.indexOf(num[i]);
+    //     // console.log(ind);
+    //     numbers = numbers.toString().split('');
+    //     // console.log('2번: ' + numbers);
+
+    //     if (ind != -1) {
+    //         var arr = numbers.splice(ind, num[i].length);
+    //         console.log(arr);
+    //         // console.log('3번: '+ numbers);
+    //         answer.push(i);
+    //     }
+    //     i++;
+    //     numbers = numbers.join('');
+    // }
+
+
+    // console.log(answer.join(''));
+
+
