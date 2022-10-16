9b8092e89fc56ed004de28b239c5e10031229b35
jooyonee <isyawn@naver.com>
2022-10-16 10:49:21 +0900

    가까운 수

--- Lv.1/prg36.js
+++ Lv.1/prg36.js
@@ -6,12 +6,12 @@ var gap = [];
 for (var i = 0; i<array.length; i++) {
     gap[i] = Math.abs(n - array[i]);
 }
-console.log(gap);
+
 var same = [];
 for (var i = 0; i<gap.length; i++) {
     if (Math.min(...gap) === gap[i]) {
         same.push(array[i]);
     }
-}
+} //처음부터 array를 오름차순으로 해주면 이 for문은 통으로 필요 없음..
 console.log(Math.min(...same));
 
