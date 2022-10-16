deef0052773639a88262ea62039b791a8473314f
jooyonee <isyawn@naver.com>
2022-10-16 22:12:02 +0900

    영어가 싫어요

--- Lv.1/prg38.js
+++ Lv.1/prg38.js
@@ -1,5 +1,32 @@
 //영어가 싫어요
 
-var num = [zero, one, two, three, four, five, six, seven, eight, nine];
+var numbers = "onefourzerosixseven";
+// var numbers = numbers.split('');
+
+var num = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+    var answer = [];
+
+    var i = 0;
+    while (numbers.length != 0) {
+        var ind = numbers.indexOf(num[i]);
+        console.log(numbers);
+
+        numbers = numbers.toString().split(',');
+
+        if (ind != -1) {
+            numbers.splice(ind, num[i].length);
+            answer.push(i);
+        }
+        i++;
+        numbers.join('');
+        console.log(numbers);
+
+    }
+console.log(answer);
+//     var ind = numbers.indexOf(num[1]);
+//     var numbers = numbers.split('');
+//     var arr = numbers.splice(ind, num[1].length);
+
     
-    for(var i = 0; )
+
+// console.log(numbers);
