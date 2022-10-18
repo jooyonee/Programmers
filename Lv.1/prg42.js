d4cbeaa9154ef43d41ec4e97e7c585b25f03d57b
jooyonee <isyawn@naver.com>
2022-10-18 23:24:02 +0900

    OX퀴즈

---
+++ Lv.1/prg42.js
@@ -0,0 +1,10 @@
+//OX퀴즈
+
+var quiz = ["19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"];
+
+var answer = [];
+    
+    for (var i = 0; i < quiz.length; i++) {
+        console.log(eval(quiz[i]));
+    }
+        
