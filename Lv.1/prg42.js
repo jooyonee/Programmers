5320b96fc4ed0c50479d423eb08f1a32a6968579
jooyonee <isyawn@naver.com>
2022-10-20 21:20:23 +0900

    OX퀴즈

--- Lv.1/prg42.js
+++ Lv.1/prg42.js
@@ -2,9 +2,25 @@
 
 var quiz = ["19 - 6 = 13", "5 + 66 = 71", "5 - 15 = 63", "3 - 1 = 2"];
 
-var answer = [];
+var answer= [];
     
     for (var i = 0; i < quiz.length; i++) {
-        console.log(eval(quiz[i]));
-    }
+        var arr = quiz[i].split(' ');
         
+        if (arr[1] == "-") {
+            answer.push(parseInt(arr[0]) - parseInt(arr[2]) == parseInt(arr[4]));
+        }
+        else if (arr[1] == "+") {
+            answer.push(parseInt(arr[0]) + parseInt(arr[2]) == parseInt(arr[4]));
+        }
+    }
+    
+    for (var i = 0; i < arr.length; i++) {
+        if (answer[i] == true) {
+            answer[i] = "O";
+        }
+        else if (answer[i] == false) {
+            answer[i] = "X";
+        }
+    }
+console.log answer;
