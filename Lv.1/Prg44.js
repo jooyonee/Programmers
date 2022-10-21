0dabb62f2d9c9f53ddbfb9c2957bc06554ec10e7
jooyonee <isyawn@naver.com>
2022-10-22 00:23:46 +0900

    잘라서 배열로 저장하기

--- Lv.1/Prg44.js
+++ Lv.1/Prg44.js
@@ -1,21 +1,17 @@
 //잘라서 배열로 저장하기
 
 var my_str = "abc1Addfggg4556b";
-var n = 1;
+var n = 4;
 
+my_str = my_str.split('');
 
 var answer = [];
     var num = parseInt(my_str.length / n);
     
-    for (i = 0; i <= num; i++) {
-        
-        var ele = [];
-        for (j = 0; j < n; j++) {
-            ele.push(my_str[i*n + j]);
-        }
-        ele = ele.filter(n => n != undefined);
-        answer.push(ele.join(''));
-        answer = answer.filter(n => n != '');
-    }
+    console.log(my_str.length);
 
-    console.log(answer);
+    for (i = 0; i < my_str.length; i+=n) {
+        console.log(i+ '/' + my_str.splice(i, n).join(''));
+    }  //왜 안 되지? -> splice가 삭제를 해버린다.
+
+    // console.log(answer);
